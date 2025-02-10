4e70cf45f0b22b70e1ec0160c51d23a5f6b2deaf
michi-okahata <michi.okahata@outlook.com>
2025-02-09 23:15:16 -0500

    card and process are close

--- card.py
+++ card.py
@@ -1,43 +1,13 @@
-import json
-
 class Card:
-    def __init__(self, tag: str = None, paragraph_list: list = None):
+    def __init__(self, tag: str = "", paragraphs: list = []):
         self.tag = tag
-        self.paragraphs = paragraph_list
+        self.paragraphs = paragraphs
 
     def to_dict(self):
         return {
             "Tag": self.tag,
-            "Paragraphs": [para.to_dict() for para in self.paragraphs]
-        }
-
-class Card_Paragraph:
-    def __init__(self, card_runs: list):
-        self.runs = card_runs
-    
-    def to_dict(self):
-        return {
-            "Runs": [run.to_dict() for run in self.runs]
-        }
-
-class Card_Run:
-    def __init__(self, run_style: str, run_text: str):
-        self.style = run_style
-        self.text = run_text
-    
-    def to_dict(self):
-        return {
-            "Style": self.style,
-            "Text": self.text
+            "Body": self.paragraphs
         }
 
 if __name__ == "__main__":
-    run1 = Card_Run("Economic decline", "Bold")
-    run2 = Card_Run("doesn't cause war.", "Italic")
-
-    paragraph = Card_Paragraph([run1, run2])
-
-    walt20 = Card("Economic decline doesn't cause war.", [paragraph])
-
-    card_dict = walt20.to_dict()
-    print(json.dumps(card_dict, indent=4))
+    print("test")
--- process.py
+++ process.py
@@ -1,71 +1,79 @@
+# import zipfile
 from lxml import etree
 import json
-from card import Card, Card_Paragraph, Card_Run
+from card import Card
 
-# Next step is to generate cards + text dumps
-# Maintain paragraph integrity
+namespaces = { "w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main" }
 
-namespaces = {"w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main"}
-
-def parse(filepath):
+# parse xml
+def parse_xml(filepath):
     # Grab document
     doc = etree.parse(filepath)
     body = doc.getroot().getchildren()[0]
 
-    # Get tags
-    '''
-    tags = {element.tag for element in body.iter()}
-    stripped_tags = {etree.QName(tag).localname for tag in tags}
-    print(stripped_tags)
-    '''
+    paragraph_elements = body.xpath(".//w:p", namespaces=namespaces)
 
-    text_elements = body.xpath(".//w:t", namespaces=namespaces)
+    cards = []
+    card = Card()
 
-    # Card might be useless, just make a dict
-    # Need to figure out how to parse paragraphs
+    for paragraph_element in paragraph_elements:
+        text_elements = paragraph_element.xpath(".//w:t", namespaces=namespaces)
+        paragraph = []
 
-    for text_element in text_elements:
-        style, text = parse_element(text_element)
-        '''
-        Iterate through each text element
-        if tag -> new card, add tag
-        if paragraph -> add paragraph
-        if run -> add run and style 
+        for text_element in text_elements:
+            text, style = parse_element(text_element)
 
-        if tag -> add a new card, new card is what is accessed
-        if paragraph -> add new paragraph to card
-        if run -> add run and style to paragraph   
-        '''
+            if style == "Tag":
+                if not card.paragraphs:
+                    card.tag += text; # add tag
+                else:
+                    cards.append(card) # add existing card
+                    card = Card(tag=text) # new card
+            
+            elif style in {"Normal/Card", "Underline", "Cite"}:
+                paragraph.append((text, style)) # add body
 
+        if paragraph:  # avoid empty paragraphs, TODO: fix tag, tag
+            card.paragraphs.append(paragraph)
 
-# Return the style and text for each text element.
+    if card.paragraphs:
+        cards.append(card) # add last card
+        
+    for card in cards:
+        print(json.dumps(card.to_dict(), indent=4))
+
+# return the style and text for each text element
 def parse_element(text_element):
     parent_element = text_element.getparent()
     style_element = parent_element.xpath(".//w:rStyle", namespaces=namespaces)
 
     if len(style_element) == 0:
-        # Grab paragraph style
+        # grab paragraph style
         parent_element = parent_element.getparent() # Grandparent.
         style_element = parent_element.xpath(".//w:pStyle", namespaces=namespaces)
 
-    style = "Normal/Card"
     text = text_element.text
+    style = "Normal/Card"
 
     if len(style_element) != 0:
         style_dump = parent_element.xpath(".//w:rStyle|.//w:pStyle", namespaces=namespaces)[0].get("{" + namespaces["w"] + "}val")
-        # Replace with a hash set of style names, e.g., emphasis v. boxes.
-        if style_dump == "Heading4": style = "Tag"
-        if style_dump == "Style13ptBold": style = "Cite"
+        # replace with a dictionary of style names, e.g., emphasis v. boxes.
         if style_dump == "StyleUnderline": style = "Underline"
         if style_dump == "Emphasis": style = "Emphasis"
+        if style_dump == "Style13ptBold": style = "Cite"
+        if style_dump == "Heading4": style = "Tag"
+        if style_dump == "Heading3": style = "Block"
+        if style_dump == "Heading2": style = "Hat"
+        if style_dump == "Heading1": style = "Pocket"
 
-    print(style + ":" + text)
-    return style, text
+    # print(text, "-", style)
+    return text, style
 
 def main():
-    # Parses output2.xml fine, re: speed for large documents
+    # parses output2.xml fine, re: speed for large documents
     # file = './cards/output2.xml'
     file = "./cards/output.xml"
-    parse(file)
+    parse_xml(file)
 
-main()
+if __name__ == "__main__":
+    main()
